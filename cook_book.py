867fbfbeedce02d36e743b649fb7a0e2360c43fd
TatianaAndreevna <Tanjaverkhvceva@rambler.ru>
2018-11-22 20:52:54 +0300

    Initial commit

---
+++ cook_book.py
@@ -0,0 +1,30 @@
+with open('cook_book.txt', encoding='UTF8') as f:
+    cook_book = {}
+    for line in f:
+        dish = line.strip()
+        quantity_ingredients = f.readline().strip()
+        ingredient_list = []
+        ingredient_dish = {}
+        for i in range(int(quantity_ingredients)):
+            ingredient = f.readline().strip().split(' | ')
+            ingredient_dish['ingredient_name'] = ingredient[0]
+            ingredient_dish['quantity'] = ingredient[1]
+            ingredient_dish['measure'] = ingredient[2]
+            ingredient_list.append(ingredient_dish.copy())
+            cook_book[dish] = ingredient_list
+        f.readline()
+
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dish in dishes:
+        for ingredient in cook_book[dish]:
+            ingredient_dish = ingredient
+            if ingredient_dish['ingredient_name'] not in shop_list:
+                shop_list[ingredient_dish['ingredient_name']] = {'measure' : ingredient_dish['measure'], 'quantity' : int(ingredient_dish['quantity']) * person_count}
+            else:
+                shop_list[ingredient_dish['ingredient_name']]['quantity'] += int(ingredient_dish['quantity']) * person_count
+    print(shop_list)
+
+
+get_shop_list_by_dishes(['Фахитос', 'Омлет'], 4)
