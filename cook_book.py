5d5741e86946d304b9a97d29583fe0f2c687b683
TatianaAndreevna <Tanjaverkhvceva@rambler.ru>
2018-11-28 22:18:47 +0300

    Исправления

--- cook_book.py
+++ cook_book.py
@@ -1,21 +1,24 @@
-with open('cook_book.txt', encoding='UTF8') as f:
+def open_cook_book():
     cook_book = {}
-    for line in f:
-        dish = line.strip()
-        quantity_ingredients = f.readline().strip()
-        ingredient_list = []
-        ingredient_dish = {}
-        for i in range(int(quantity_ingredients)):
-            ingredient = f.readline().strip().split(' | ')
-            ingredient_dish['ingredient_name'] = ingredient[0]
-            ingredient_dish['quantity'] = ingredient[1]
-            ingredient_dish['measure'] = ingredient[2]
-            ingredient_list.append(ingredient_dish.copy())
-            cook_book[dish] = ingredient_list
-        f.readline()
+    with open('cook_book.txt', encoding='UTF8') as f:
+        for line in f:
+            dish = line.strip()
+            quantity_ingredients = f.readline().strip()
+            ingredient_list = []
+            ingredient_dish = {}
+            for i in range(int(quantity_ingredients)):
+                ingredient = f.readline().strip().split(' | ')
+                ingredient_dish['ingredient_name'] = ingredient[0]
+                ingredient_dish['quantity'] = ingredient[1]
+                ingredient_dish['measure'] = ingredient[2]
+                ingredient_list.append(ingredient_dish.copy())
+                cook_book[dish] = ingredient_list
+            f.readline()
+    return cook_book
 
 
 def get_shop_list_by_dishes(dishes, person_count):
+    cook_book = open_cook_book()
     shop_list = {}
     for dish in dishes:
         for ingredient in cook_book[dish]:
